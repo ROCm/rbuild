61517ad34a8f8c59c9ce26dbe8231027f4508715
Paul <pfultz2@yahoo.com>
2019-01-27 13:47:51 -0600

    Initial commit

---
+++ rbuild/cli.py
@@ -0,0 +1,76 @@
+import subprocess, click, os, shutil, multiprocessing
+
+from rbuild import __version__
+
+ignore_deps = [
+    'danmar/cppcheck',
+    'RadeonOpenCompute/clang-ocl',
+    'RadeonOpenCompute/rocm-cmake',
+    'ROCm-Developer-Tools/HIP',
+    'ROCmSoftwarePlatform/MIOpen',
+    'ROCmSoftwarePlatform/MIOpenGEMM',
+    'ROCmSoftwarePlatform/rocBLAS'
+]
+
+def mkdir(p):
+    if not os.path.exists(p): os.makedirs(p)
+    return p
+
+def delete_dir(path):
+    if path is not None and os.path.exists(path): shutil.rmtree(path)
+
+def make_args(**kwargs):
+    args = []
+    for key, value in kwargs.items():
+        if value:
+            name = '--' + key
+            for arg in value if isinstance(value, list) else [value]:
+                args = args + [name, arg]
+    return args
+
+def make_defines(defines):
+    return ['-D'+x for x in defines]
+
+def cget(prefix):
+    def f(*args, **kwargs):
+        subprocess.check_call(['cget', '-p', prefix] + args, **kwargs)
+    return f
+
+def cmake(*args, **kwargs):
+    subprocess.check_call(['cmake'] + args, **kwargs)
+
+def make(target, build='.'):
+    args = make_args(build=build, config='Release', target=target) + ['--' + '-j' + str(multiprocessing.cpu_count())]
+    cmake(args)
+
+@click.group(context_settings={'help_option_names': ['-h', '--help']})
+@click.version_option(version=__version__, prog_name='rbuild')
+def cli(branch, url, path):
+    pass
+
+@cli.command()
+@click.option('-d', '--deps-dir', required=True, help="Directory for the third-party dependencies")
+@click.option('-S', '--source-dir', required=False, help="Directory of the source code")
+@click.option('-t', '--toolchain', required=False, help="Set cmake toolchain file to use")
+@click.option('--cxx', required=False, help="Set c++ compiler")
+@click.option('-D', '--define', multiple=True, help="Extra cmake variables to add to the toolchain")
+def prepare(deps_dir, source_dir, toolchain, cxx, define):
+    cg = cget(deps_dir)
+    cg('init', *make_args(cxx=cxx, toolchain=toolchain, define=define))
+    for dep in ignore_deps:
+        cg('ignore', dep)
+    src = source_dir or os.getcwd()
+    cg('install', cwd=src)
+
+@cli.command()
+@click.option('-d', '--deps-dir', required=True, help="Directory for the third-party dependencies")
+@click.option('-S', '--source-dir', required=False, help="Directory of the source code")
+@click.option('-B', '--build-dir', required=True, help="Directory for the build")
+@click.option('-D', '--define', multiple=True, help="Extra cmake variables to add to the toolchain")
+def package(deps_dir, source_dir, build_dir, define):
+    toolchain = os.path.join(deps_dir, 'cget', 'cget.cmake')
+    src = source_dir or os.getcwd()
+    mkdir(build_dir)
+    cmake('-DCMAKE_TOOLCHAIN_FILE='+toolchain, src, *make_defines(define), cwd=build_dir)
+    make('package', build=build_dir)
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os, re
+from setuptools import setup, find_packages
+
+def get_version(package):
+    """Return package version as listed in `__version__` in `init.py`."""
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+def get_requires(filename):
+    requirements = []
+    with open(filename) as req_file:
+        for line in req_file.read().splitlines():
+            if not line.strip().startswith("#"):
+                requirements.append(line)
+    return requirements
+
+project_requirements = get_requires("requirements.txt")
+
+setup(
+    name="rbuild",
+    version=get_version("rbuild"),
+    url='https://github.com/pfultz2/rbuild',
+    license='boost',
+    description='Rocm build',
+    author='Paul Fultz II',
+    author_email='pfultz2@yahoo.com',
+    packages=find_packages(),
+    install_requires=project_requirements,
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'rbuild = rbuild.cli:cli',
+        ]
+    },
+    zip_safe=False
+)
