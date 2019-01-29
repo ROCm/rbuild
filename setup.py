22c4dcd644211a8b7ddf08a19d6dca07b4c05dc3
Paul <pfultz2@yahoo.com>
2019-01-28 23:01:25 -0600

    Install correct cget version

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-import os, re
+import os, re, subprocess, sys
 from setuptools import setup, find_packages
+from setuptools.command.install import install as InstallCommand
+
+def pip_install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
 
 def get_version(package):
     """Return package version as listed in `__version__` in `init.py`."""
@@ -16,7 +20,9 @@ def parse_requirements(filename):
 def get_requires(filename):
     for x in parse_requirements(filename):
         if '#egg=' in x:
-            yield x[x.find('#egg=')+5:]
+            start = x.find('#egg=')+5
+            pkg = x[start:]
+            yield pkg
         else:
             yield x
 
@@ -26,6 +32,14 @@ def get_dependency_links(filename):
 install_requires = get_requires("requirements.txt")
 dependency_links = get_dependency_links("requirements.txt")
 
+class Install(InstallCommand):
+    """ Customized setuptools install command which uses pip. """
+
+    def run(self, *args, **kwargs):
+        for x in dependency_links:
+            pip_install(x)
+        InstallCommand.run(self, *args, **kwargs)
+
 setup(
     name="rbuild",
     version=get_version("rbuild"),
@@ -34,9 +48,11 @@ setup(
     description='Rocm build',
     author='Paul Fultz II',
     author_email='pfultz2@yahoo.com',
+    cmdclass={
+        'install': Install,
+    },
     packages=find_packages(),
     install_requires=install_requires,
-    dependency_links=dependency_links,
     include_package_data=True,
     entry_points={
         'console_scripts': [
