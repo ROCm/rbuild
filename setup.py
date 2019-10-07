8a4811dcdd1efa9138eecd7b9b795acd7b5c9f3b
Paul <pfultz2@yahoo.com>
2019-10-07 09:33:50 -0500

    Simplify requirements.txt

--- setup.py
+++ setup.py
@@ -10,35 +10,15 @@ def get_version(package):
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
-def parse_requirements(filename):
+def get_requires(filename):
+    requirements = []
     with open(filename) as req_file:
         for line in req_file.read().splitlines():
-            s = line.strip()
-            if not s.startswith("#"):
-                yield s
+            if not line.strip().startswith("#"):
+                requirements.append(line)
+    return requirements
 
-def get_requires(filename):
-    for x in parse_requirements(filename):
-        if '#egg=' in x:
-            start = x.find('#egg=')+5
-            pkg = x[start:]
-            yield pkg
-        else:
-            yield x
-
-def get_dependency_links(filename):
-    return [x for x in parse_requirements(filename) if '#egg=' in x]
-
-install_requires = get_requires("requirements.txt")
-dependency_links = get_dependency_links("requirements.txt")
-
-class Install(InstallCommand):
-    """ Customized setuptools install command which uses pip. """
-
-    def run(self, *args, **kwargs):
-        for x in dependency_links:
-            pip_install(x)
-        InstallCommand.run(self, *args, **kwargs)
+project_requirements = get_requires("requirements.txt")
 
 setup(
     name="rbuild",
